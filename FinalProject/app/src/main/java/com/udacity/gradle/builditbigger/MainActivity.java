6842405186957ef82a853ee4a02ab3a072137924
Daniel W <dmachine777@gmail.com>
2019-08-28 20:49:12 -0500

    step 3 works. endpoint gives result to android library.

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.udacity.gradle.jokeandroidlibrarywithactivity.JokeActivity;
-import com.udacity.gradle.jokejavalibrary.JokeHolderClass;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,49 +18,27 @@ public class MainActivity extends AppCompatActivity {
           setContentView(R.layout.activity_main);
      }
 
-
-     @Override
-     public boolean onCreateOptionsMenu(Menu menu) {
-          // Inflate the menu; this adds items to the action bar if it is present.
-          getMenuInflater().inflate(R.menu.menu_main, menu);
-          return true;
-     }
-
-     @Override
-     public boolean onOptionsItemSelected(MenuItem item) {
-          // Handle action bar item clicks here. The action bar will
-          // automatically handle clicks on the Home/Up button, so long
-          // as you specify a parent activity in AndroidManifest.xml.
-          int id = item.getItemId();
-
-          //noinspection SimplifiableIfStatement
-          if (id == R.id.action_settings) {
-               return true;
-          }
-
-          return super.onOptionsItemSelected(item);
-     }
-
-     public void tellJoke(View view) {
-
-          JokeHolderClass jokes = new JokeHolderClass();
-          String randomJoke = jokes.retrieveJokeFromJavaLibrary();
-          Toast.makeText(this, randomJoke, Toast.LENGTH_SHORT).show();
-          Intent myIntent = new Intent(getApplicationContext(), JokeActivity.class);
-          myIntent.putExtra(JokeActivity.KEY, randomJoke);
-          startActivity(myIntent);
-     }
-
-
-     //public void tellJoke(View view) {
-//          new ASyncTaskToEndpoints() {
-//               @Override
-//               protected void onPostExecute(String result) {
-//                    Intent myIntent = new Intent(getApplicationContext(), JokeActivity.class);
-//                    myIntent.putExtra(JokeActivity.KEY, result);
+//     public void tellJoke(View view) {
 //
-//                    startActivity(myIntent);
-//               }
-//          }
+//          JokeHolderClass jokes = new JokeHolderClass();
+//          String randomJoke = jokes.retrieveJokeFromJavaLibrary();
+//          Toast.makeText(this, randomJoke, Toast.LENGTH_SHORT).show();
+//          Intent myIntent = new Intent(getApplicationContext(), JokeActivity.class);
+//          myIntent.putExtra(JokeActivity.KEY, randomJoke);
+//          startActivity(myIntent);
+//     }
 
+
+     @SuppressLint("StaticFieldLeak")
+     public void tellJoke(View view) {
+          new EndpointsAsyncTask() {
+               @Override
+               protected void onPostExecute(String result) {
+                    Intent myIntent = new Intent(getApplicationContext(), JokeActivity.class);
+                    myIntent.putExtra(JokeActivity.KEY, result);
+                    startActivity(myIntent);
+               }
+          }.execute();
+
+     }
 }
